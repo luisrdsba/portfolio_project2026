6c19b778cd2deaa3b72792f165a881faf1136206
Luis Almoster <luisrebelouni@gmail.com>
2026-05-14 22:29:33 +0100

    Adicionar CRUD de artigos por autor

--- artigos/urls.py
+++ artigos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.artigos_lista, name='artigos_lista'),
+    path('novo/', views.novo_artigo, name='novo_artigo'),
+    path('<int:artigo_id>/', views.artigo_detalhe, name='artigo_detalhe'),
+    path('<int:artigo_id>/edita/', views.edita_artigo, name='edita_artigo'),
+    path('<int:artigo_id>/apaga/', views.apaga_artigo, name='apaga_artigo'),
 ]
--- artigos/views.py
+++ artigos/views.py
@@ -1,7 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Artigo
+from .forms import ArtigoForm
+
+
+def is_autor(user):
+    return user.is_authenticated and user.groups.filter(name='autores').exists()
 
 
 def artigos_lista(request):
     artigos = Artigo.objects.select_related('autor').all()
-    return render(request, 'artigos/lista.html', {'artigos': artigos})
+    return render(request, 'artigos/lista.html', {
+        'artigos': artigos,
+        'is_autor': is_autor(request.user),
+    })
+
+
+def artigo_detalhe(request, artigo_id):
+    artigo = get_object_or_404(Artigo, id=artigo_id)
+    pode_editar = request.user.is_authenticated and artigo.autor == request.user
+    return render(request, 'artigos/detalhe.html', {
+        'artigo': artigo,
+        'pode_editar': pode_editar,
+    })
+
+
+@login_required
+def novo_artigo(request):
+    if not is_autor(request.user):
+        return redirect('artigos_lista')
+    if request.method == 'POST':
+        form = ArtigoForm(request.POST, request.FILES)
+        if form.is_valid():
+            artigo = form.save(commit=False)
+            artigo.autor = request.user
+            artigo.save()
+            return redirect('artigo_detalhe', artigo_id=artigo.id)
+    else:
+        form = ArtigoForm()
+    return render(request, 'artigos/novo.html', {'form': form})
+
+
+@login_required
+def edita_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, id=artigo_id)
+    if artigo.autor != request.user:
+        return redirect('artigos_lista')
+    if request.method == 'POST':
+        form = ArtigoForm(request.POST, request.FILES, instance=artigo)
+        if form.is_valid():
+            form.save()
+            return redirect('artigo_detalhe', artigo_id=artigo.id)
+    else:
+        form = ArtigoForm(instance=artigo)
+    return render(request, 'artigos/edita.html', {'form': form, 'artigo': artigo})
+
+
+@login_required
+def apaga_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, id=artigo_id)
+    if artigo.autor != request.user:
+        return redirect('artigos_lista')
+    artigo.delete()
+    return redirect('artigos_lista')
